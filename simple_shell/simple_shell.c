07b0f7e897e0e3a021b4c08f553fd066243aaea7
Blair Fitzhugh <bfitz1@umbc.edu>
2022-03-12 18:02:19 -0500

    Start exit builtin

--- simple_shell/simple_shell.c
+++ simple_shell/simple_shell.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+
+#define PROMPT "shell> "
+#define MAX_BUFFER_LENGTH 256
 
 int main(int argc, char* argv[]) {
     if (argc > 1) {
@@ -6,7 +10,23 @@ int main(int argc, char* argv[]) {
 	return 1;
     }
 
-    printf("Hello, world!\n");
+    printf(PROMPT);
 
+    char buffer[MAX_BUFFER_LENGTH];
+
+    unsigned buflen = 0;
+    do {
+        buffer[buflen] = getc(stdin);
+	buflen += 1;
+    } while (buflen < MAX_BUFFER_LENGTH && buffer[buflen-1] != 0x0a);
+    buffer[buflen-1] = 0x00;
+
+    if (strcmp(buffer, "exit") != 0) {
+        printf("Command other than exit.\n");
+    } else {
+        printf("Exit called.\n");
+    }
+
+    printf("Goodbye.\n");
     return 0;
 }
