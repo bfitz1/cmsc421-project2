bfaa68c8eeffcd62ab7cf51de3df3965d0743b5a
Blair Fitzhugh <bfitz1@umbc.edu>
2022-03-13 15:55:48 -0400

    Refactor underway

--- simple_shell/simple_shell.c
+++ simple_shell/simple_shell.c
@@ -1,139 +1,147 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define PROMPT "shell> "
-#define MAX_BUFFER_LENGTH 256
+#include "utils.h"
 
-typedef struct Command {
+#define PROMPT "$ "
+#define BUFFER_LENGTH 4096
+
+typedef struct argdata_t {
+    char* argstring;
+    unsigned int arglength;
+    char** arglist;
+    unsigned int argcount;
+} argdata_t;
+
+typedef struct command_t {
     enum {
-        CommandTag_Absolute,
-        CommandTag_User,
-        CommandTag_Relative,
-        CommandTag_Builtin,
+        kind_process,
+        kind_procfs,
+        kind_exit,
+        kind_invalid,
     } tag;
 
-    int argc;
-    char* argv[MAX_BUFFER_LENGTH];
-} Command;
+    union {
+        char** list;
+        char* path;
+        int code;
+        char* input;
+    };
+} command_t;
 
-Command ParseCommand(char* input) {
-    // Just testing things for now
-    
-    return
-        /*
-        (Command) {
-            .tag = CommandTag_User,
-            .argc = 1,
-            .argv = {"ls", NULL},
-        };
-        */
-        /*
-        (Command) {
-            .tag = CommandTag_Absolute,
-            .argc = 2
-            .argv = {"/bin/ls", "-la", NULL},
-        };
-        */
-        (Command) {
-            .tag = CommandTag_Builtin,
-            .argc = 2,
-            .argv = {"proc", "1/status", "NULL"},
-        };
-        /*
-        (Command) {
-            .tag = CommandTag_Builtin,
-            .argc = 1,
-            .argv = {"exit"}
-        };
-        */
-        /*
-        (Command) {
-            .tag = CommandTag_Builtin,
-            .argc = 2,
-            .argv = {"exit", "9"}
-        };
-        */
+argdata_t* argdata_make(char* raw, unsigned int len) {
+    argdata_t* arg = malloc(sizeof(argdata_t*));
+
+    if (arg == NULL) {
+        perror("Error: Couldn't allocate argdata");
+        exit(1);
+    }
+
+    arg->argstring = strdup(raw);
+    arg->arglength = len;
+    arg->arglist = NULL; // Figure this out
+    arg->argcount = 0;
+
+    return arg;
+}
+
+void argdata_free(argdata_t* arg) {
+    free(arg->argstring);
+    for (unsigned int i = 0; i < arg->argcount; i += 1) {
+        free(arg->arglist[i]);
+    }
+    free(arg);
+}
+
+command_t* command_make(argdata_t* arg) {
+    command_t* cmd = malloc(sizeof(command_t*));
+
+    // It's just exit for now
+    cmd->tag = kind_exit;
+    cmd->code = 0;
+
+    return cmd;
+}
+
+void command_free(command_t* cmd) {
+    switch (cmd->tag) {
+        case kind_process:
+            break;
+        case kind_procfs:
+            break;
+        case kind_exit:
+            break;
+        case kind_invalid:
+            break;
+        default:
+            perror("Error: Unreachable at command_free");
+            break;
+    }
+
+    free(cmd);
 }
 
 int main(int argc, char* argv[]) {
 
     if (argc > 1) {
-        perror("Don't call me with any arguments.");
+        perror("Error: Don't call me with any arguments.\n");
         return 1;
     }
 
-    char buffer[MAX_BUFFER_LENGTH];
+    char buffer[BUFFER_LENGTH];
+    // Test data
+    // command_t cmd = (command_t) { .tag = kind_exit, .code = 7 };
+    // command_t cmd = (command_t) { .tag = kind_procfs, .path = "/proc/1/status" };
+    // command_t cmd = (command_t) { .tag = kind_invalid, .input = "woops" };
+    char** arglist = malloc(sizeof(char*) * 3);
+    arglist[0] = "/bin/ls";
+    arglist[1] = "-la";
+    arglist[2] = NULL;
+    command_t cmd = (command_t) { .tag = kind_process, .list = arglist };
 
     while (1) {
         printf(PROMPT);
+        fgets(buffer, BUFFER_LENGTH, stdin);
 
-        unsigned buflen = 0;
-        do {
-            buffer[buflen] = getc(stdin);
-            buflen += 1;
-        } while (buflen < MAX_BUFFER_LENGTH && buffer[buflen-1] != 0x0a);
-        buffer[buflen-1] = 0x00;
-
-        Command cmd = ParseCommand(buffer);
-        switch (cmd.tag) {
-            case CommandTag_Absolute:
-            case CommandTag_User:
-            case CommandTag_Relative:
-                printf("It is a command!\n");
-
-                int pid = fork();
-                if (pid > 0) {
-                    wait(NULL);
-                } else if (pid == 0) {
-                    execvp(cmd.argv[0], cmd.argv);
-                    perror("Failed at exec!");
-                    exit(1);
-                } else {
-                    perror("Failed at fork!");
-                }
-                break;
-
-            case CommandTag_Builtin:
-                printf("It is a builtin!\n");
-                if (strcmp(cmd.argv[0], "proc") == 0) {
-                    printf("It is a proc call!\n");
-                    snprintf(buffer, (size_t) MAX_BUFFER_LENGTH, "/%s/%s", cmd.argv[0], cmd.argv[1]);
-                    printf("Reading from %s\n", buffer);
-                    FILE* proc = fopen("/proc/1/status", "r");
-                    int c;
-                    while ((c = fgetc(proc)) != EOF) {
-                        printf("%c", c);
-                    }
-                    fclose(proc);
-                } else if (strcmp(cmd.argv[0], "exit") == 0) {
-                    printf("It is an exit call!\n");
-                    if (cmd.argc == 1) {
-                        exit(0);
-                    } else if (isdigit(cmd.argv[1][0])) {
-                       // Remember to free any allocated memory!
-                       exit(atoi(cmd.argv[1])); 
-                    }
-                } else {
-                    perror("Failed at builtin!");
-                    return 1;
-                }
-                break;
-
-            default:
-                fprintf(stderr, "This shouldn't happen!\n");
+        if (cmd.tag == kind_process) {
+            pid_t pid = fork();
+            if (pid > 0) {
+                wait(NULL);
+            } else if (pid == 0) {
+                execvp(cmd.list[0], cmd.list);
+            } else {
+                perror("Error: Couldn't fork the process");
                 return 1;
+            }    
+        } else if (cmd.tag == kind_procfs) {
+            FILE* fd = fopen(cmd.path, "r");
+            if (fd == NULL) {
+                fprintf(stderr, "Error: Couldn't open %s\n", cmd.path);
+                return 1;
+            }
+            int byte;
+            while ((byte = fgetc(fd)) != EOF) {
+                printf("%c", byte);
+            }
+            fclose(fd);
+        } else if (cmd.tag == kind_exit) {
+            exit(cmd.code);
+        } else if (cmd.tag == kind_invalid) {
+            // Just move on
+            printf("Debugging: %s\n", cmd.input);
+        } else {
+            fprintf(stderr, "Error: Couldn't match on command");
+            return 1;
         }
-
-        
-        printf("You called: %s\n", cmd.argv[0]);
         break;
     }
 
-    printf("Goodbye.\n");
+    //free(arglist);
+    printf("Exiting. Goodbye!\n");
     return 0;
 }
